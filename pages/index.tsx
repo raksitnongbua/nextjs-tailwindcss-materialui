3d14d945ac069703198e584ed7efb2262f068254
Raksit Nongbua <tan.raksit@bitkub.com>
2021-03-03 11:58:31 +0700

    feat: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
+    <div className='p-4'>
       <Container maxWidth='lg'>
         <Typography> This is Home Screen</Typography>
       </Container>
