6062010e07acdb16394b1a0193970a846ed48bb3
Raksit Nongbua <tan.raksit@bitkub.com>
2021-03-05 14:31:30 +0700

    feat: upgrade material ui from 4.11 to 5.0-alpha

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // primary: 'var(--primary-color);',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-  corePlugins: {
-    preflight: false,
-  },
 };
