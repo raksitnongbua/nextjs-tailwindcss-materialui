167b5b4df8be0957c3fe11da74f614d615d431b4
Raksit Nongbua <tan.raksit@bitkub.com>
2021-03-08 12:10:23 +0700

    add spend system into buy form

--- components/buyForm.tsx
+++ components/buyForm.tsx
@@ -6,13 +6,37 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+
+interface IProps {
+  onBuy?: (spend: number) => void;
+}
 
 const AVAILABLE_BALANCE_STR = 'ยอดคงเหลือ';
 const FEE_CREDIT_STR = 'เครดิตค่าธรรมเนียม';
 const YOU_SPEND_STR = 'จำนวนที่ต้องจ่าย';
 const CURRENCY_STR = 'THB';
-const BuyForm = () => {
+const BUY_STR = 'ซื้อ';
+
+const BuyForm: React.FC<IProps> = ({ onBuy }) => {
+  const feeCredit = 0;
+  const balance = 100000;
+  const [spend, setSpend] = useState<number>(0);
+  const [spendPercent, setSpendPercent] = useState<number>(0);
+  const handleSpendChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const possibleValue = Math.min(Math.max(+event.target.value, 0), balance);
+    setSpend(possibleValue);
+    setSpendPercent((possibleValue / balance) * 100);
+  };
+  const handleSpendPercentChange = (_event: any, value: number) => {
+    const percent = Math.round(value);
+    setSpendPercent(percent);
+    const spend = +((percent / 100) * balance).toFixed(2);
+    setSpend(spend);
+  };
+  const handleBuyClick = () => {
+    if (onBuy) onBuy(spend);
+  };
   return (
     <div>
       <div className='w-72 flex justify-between'>
@@ -20,7 +44,8 @@ const BuyForm = () => {
           {AVAILABLE_BALANCE_STR} {CURRENCY_STR}
         </Typography>
         <div className='w-24 flex justify-end align-middle'>
-          <Skeleton className='w-14' />
+          {/* <Skeleton className='w-14' /> */}
+          <Typography>{balance}</Typography>
           <Typography className='ml-2'>{CURRENCY_STR}</Typography>
         </div>
       </div>
@@ -28,7 +53,7 @@ const BuyForm = () => {
         <Typography>{FEE_CREDIT_STR}</Typography>
         <div className='w-24 flex justify-end align-middle'>
           <Typography className='text-yellow-500' align='right'>
-            0
+            {feeCredit}
           </Typography>
           <Typography className='ml-2'>{CURRENCY_STR}</Typography>
         </div>
@@ -38,6 +63,8 @@ const BuyForm = () => {
           size='small'
           type='number'
           placeholder='0.00'
+          value={spend || ''}
+          onChange={handleSpendChange}
           inputProps={{ min: 0, style: { textAlign: 'right' } }}
           InputProps={{
             endAdornment: (
@@ -54,9 +81,15 @@ const BuyForm = () => {
           valueLabelDisplay='auto'
           valueLabelFormat={(x) => `${x}%`}
           className='w-72'
+          value={spendPercent}
+          onChange={handleSpendPercentChange}
         />
-        <Button variant='contained' className='text-black dark:text-white'>
-          ซื้อ
+        <Button
+          variant='contained'
+          onClick={handleBuyClick}
+          className='text-black dark:text-white'
+        >
+          {BUY_STR}
         </Button>
       </div>
     </div>
